84d9ca7443ce412cc42f3f81cdd1a1bec39ce504
BuildTools <unconfigured@null.spigotmc.org>
2022-02-20 12:23:52 +0100

    commit

---
+++ src/me/bagulec/Main.java
@@ -0,0 +1,13 @@
+package me.bagulec;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
